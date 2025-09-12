c36c37ea8b98b55a03e2bfd1780b04279ed7f4f6
yenmangu <robert.shelford@googlemail.com>
2025-09-12 11:55:24 +0100

    fix(init.js): remove test api calls from non app bootstrap

--- src/init.js
+++ src/init.js
@@ -2,24 +2,8 @@ import { SpoonacularClient } from './api/client.js';
 import { initNavbar } from './global-ui/navbar.js';
 
 export async function initPages() {
+	initNavbar();
 	try {
-		initNavbar();
-		const client = new SpoonacularClient();
-		const data = await client.findByIngredients(
-			['apples', 'flour', 'sugar'],
-			2
-		);
-		if (data) {
-			console.log('🍽️ Recipes found:', data);
-		}
-
-		const recipes = await client.searchRecipes(['pasta'], {
-			cuisine: 'italian',
-			number: 3
-		});
-		if (recipes) {
-			console.log('Recipes Found for searchRecipes: ', recipes);
-		}
 	} catch (error) {
 		console.error('Error during init.', error);
 	}
