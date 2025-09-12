655607151ac0851e5ce4d87ab6773e0b9a20dde7
yenmangu <robert.shelford@googlemail.com>
2025-09-12 12:00:45 +0100

    debug(init): logging init calls on live site for debugging

--- src/init.js
+++ src/init.js
@@ -2,13 +2,15 @@ import { SpoonacularClient } from './api/client.js';
 import { initNavbar } from './global-ui/navbar.js';
 
 export async function initPages() {
-	initNavbar();
 	try {
+		initNavbar();
 	} catch (error) {
 		console.error('Error during init.', error);
 	}
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
+	console.log('awaiting init pages');
+
 	await initPages();
 });
