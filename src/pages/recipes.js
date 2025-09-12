5013eb586bc2eaa8c44015f0b88fefe11f201a81
yenmangu <robert.shelford@googlemail.com>
2025-09-12 11:31:47 +0100

    refactor(recipes.js): update initRecipesPage to follow interface parameter convention

--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -1,7 +1,17 @@
+/**
+ * @typedef {import('../types/componentTypes.js').RecipeGridParams} RecipeGridParams
+ */
+
 import { multipleRecipes } from '../data/multipleRecipes.js';
 import { RecipeGrid } from '../components/recipe-grid/recipeGrid.controller.js';
 
-export function initRecipesPage() {
-	const grid = new RecipeGrid(multipleRecipes);
+/**
+ *
+ * @param {HTMLElement} appRoot
+ * @param {RecipeGridParams} [params={}]
+ * @param {string} [path='']
+ */
+export function initRecipesPage(appRoot, params, path = '') {
+	const grid = new RecipeGrid(appRoot, multipleRecipes);
 	grid.render();
 }
