253b2656eed655bcbac963ad29ce35606372edf1
yenmangu <robert.shelford@googlemail.com>
2025-09-15 15:53:13 +0100

    feat(env): add deployable environment; auto switches on hostname

---
+++ src/env.js
@@ -0,0 +1,13 @@
+const PROD_HOSTNAMES = ['yenmangu.github.io'];
+
+const env = {
+	prod: {
+		API_URL: 'https://ci-cayenne-proxy.vercel.app/api'
+	},
+	dev: {
+		API_URL: 'http://127.0.0.1:3000/api'
+	}
+};
+
+const isProd = PROD_HOSTNAMES.includes(window.location.hostname);
+export const ENV = isProd ? env.prod : env.dev;
