0f2c0ed7dc26f376d95c7640e542ff3e4ed58081
yenmangu <robert.shelford@googlemail.com>
2025-09-21 22:29:28 +0100

    dev: modify env to always use remote proxy

--- src/env.js
+++ src/env.js
@@ -2,12 +2,13 @@ const PROD_HOSTNAMES = ['yenmangu.github.io'];
 
 const env = {
 	prod: {
-		API_URL: 'https://ci-cayenne-proxy.vercel.app/api'
+		API_URL: 'https://ci-cayenne-proxy.vercel.app/api/recipes'
 	},
 	dev: {
-		API_URL: 'http://127.0.0.1:3000/api'
+		API_URL: 'http://127.0.0.1:3000/api/recipes'
 	}
 };
 
 const isProd = PROD_HOSTNAMES.includes(window.location.hostname);
-export const ENV = isProd ? env.prod : env.dev;
+// export const ENV = isProd ? env.prod : env.dev;
+export const ENV = env.prod;
