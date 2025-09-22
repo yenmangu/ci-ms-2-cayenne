5e7b3655f37e68dc43481fa566de358b1be96864
yenmangu <robert.shelford@googlemail.com>
2025-09-22 23:11:24 +0100

    feat(util): implement util to convert html string to htmlElement

---
+++ src/util/htmlToElement.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * @param {string} htmlString
+ * @returns {HTMLElement}
+ * @throws {Error}
+ */
+function htmlStringToHtmlElement(htmlString) {
+	const template = document.createElement('template');
+	template.innerHTML = htmlString.trim();
+	const node = template.content.firstElementChild;
+	// debugger;
+	if (node instanceof HTMLElement) {
+		return node;
+	}
+	throw new Error('Error - node not instance of HTMLElement');
+}
+export { htmlStringToHtmlElement as stringToHtml };
