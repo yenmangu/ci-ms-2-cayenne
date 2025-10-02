e49874a09538a16ec2b88d81bc9cb5e0a77e67fd
yenmangu <robert.shelford@googlemail.com>
2025-10-02 23:22:16 +0100

    feat(response types): add response types

---
+++ src/types/responseTypes.js
@@ -0,0 +1,28 @@
+export {};
+
+/**
+ * @typedef {import("./recipeTypes.js").RecipeCard} RecipeCard
+ */
+
+/**
+ * @typedef {object} RawSearchResponse
+ * @property {RecipeCard[]} [results]
+ * @property {number} [offset] - 'Offset' specified in query
+ * @property {number} [number] - 'Number' specified in query
+ * @property {number} [totalResults] - Number of matching recipes available
+ * // NOTE: May include additional properties of any key/value
+ */
+
+/**
+ * @typedef {{[key:string]: any}} AdditionalProps
+ */
+
+/**
+ * @typedef {{
+ * 	results?: RecipeCard[],
+ * 	offset?: number,
+ * 	number?: number,
+ * 	totalResults?: number,
+ * 	[key:string]: any
+ * }} SearchResponse
+ */
