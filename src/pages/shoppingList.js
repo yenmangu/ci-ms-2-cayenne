9a9eda1b267be38722ffd1d2ff6bbd6b063c0877
yenmangu <robert.shelford@googlemail.com>
2025-10-06 23:35:44 +0100

    feat(shoppingList): add domain level logic for shopping-list feature

---
+++ src/pages/shoppingList.js
@@ -0,0 +1,17 @@
+/**
+ * @typedef {import('../types/routerTypes.js').RouteHandler} Handler
+ */
+
+import { ShoppingList } from '../components/shopping-list/shoppingList.controller.js';
+
+/**
+ * @type {Handler}
+ * @param {HTMLElement} appRoot
+ * @param {string} pathName
+ * @param {object} [params={}]
+ * @param {boolean} [params.dev=false]
+ */
+export function loadShoppingList(appRoot, pathName, params = {}) {
+	appRoot.innerHTML = '';
+	const shoppingList = new ShoppingList(appRoot, params);
+}
