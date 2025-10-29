f317dbe3a42da58d236379d93d8a12241b20357e
yenmangu <robert.shelford@googlemail.com>
2025-10-29 02:31:23 +0000

    feat(app-404): start app 404 route page

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+/**
+ * @typedef {import("../types/routerTypes.js").RouteHandler} Handler
+ */
+/**
+ * 404 page route handler method
+ */
+export function load404Page(appRoot, pathName, params = {}) {
+	const notFoundString = buildNotFoundString();
+}
+
+function buildNotFoundString() {
+	return ``;
+}
