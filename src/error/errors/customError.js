d9760f1ff503b21928a8891fad3d3e0151fa85b6
yenmangu <robert.shelford@googlemail.com>
2025-11-06 13:18:26 +0000

    feat(custom error classes): add custom error classes for consistent error throwing; classes all extend Error super class

---
+++ src/error/errors/customError.js
@@ -0,0 +1,40 @@
+/** @typedef {import("../../types/stateTypes.js").ErrorMeta} ErrorMeta */
+
+export class CustomError extends Error {
+	/**
+	 *
+	 * @param {string} message
+	 * @param {ErrorMeta|{}} [meta={}]
+	 */
+	constructor(message, meta = {}) {
+		super(message);
+		this.name = new.target.name;
+		/** @type {ErrorMeta | {}} */
+		this.meta = meta;
+		if (Object.setPrototypeOf) {
+			Object.setPrototypeOf(this, new.target.prototype);
+		}
+		if (typeof Error.captureStackTrace === 'function') {
+			Error.captureStackTrace(this, new.target);
+		}
+	}
+
+	toJSON() {
+		const { name, message, stack } = this;
+		return { name, message, stack, meta: this.meta };
+	}
+}
+
+export class AbortError extends CustomError {
+	/** @param {ErrorMeta|{}} meta */
+	constructor(meta = {}) {
+		super('[AbortError] Request aborted/timed out.', meta);
+	}
+}
+
+export class NetworkError extends CustomError {
+	/** @param {ErrorMeta|{}} meta */
+	constructor(meta = {}) {
+		super('[NetworkError] Failed to fetch', meta);
+	}
+}
---
+++ src/error/errors/httpError.js
@@ -0,0 +1,64 @@
+/**
+ * Resources:
+ * https://stackoverflow.com/questions/1382107/whats-a-good-way-to-extend-error-in-javascript
+ *
+ */
+
+import { CustomError } from './customError.js';
+
+/** @typedef {import("../../types/stateTypes.js").ErrorMeta} ErrorMeta */
+
+export class HttpError extends CustomError {
+	/**
+	 *
+	 * @param {Response|{}|undefined} [response]
+	 * @param {{status?: number, detail?:string, meta?:ErrorMeta}} [info]
+	 */
+	constructor(response, { status, detail, meta } = {}) {
+		const isHttp = response instanceof Response;
+		const resolvedStatus = isHttp ? response.status : status;
+
+		const finalMsg = isHttp
+			? `[HTTP ${response.status}] ${response.statusText}${
+					detail ? ` - ${detail}` : ''
+			  }`
+			: typeof resolvedStatus === 'number'
+			? `[HTTP ${resolvedStatus}]${detail ? ` - ${detail}` : ''}`
+			: 'Unknown HTTP Error';
+
+		super(finalMsg);
+
+		/** @type {string} */
+		this.name = 'HttpError';
+
+		/** @type {string|undefined} */
+		this.detail = detail;
+
+		/** @type {ErrorMeta|undefined} */
+		this.meta = meta;
+
+		/** @type {number|undefined} */
+		this.status = resolvedStatus;
+
+		// Tooling/Minification guard
+		if (Object.setPrototypeOf) {
+			Object.setPrototypeOf(this, new.target.prototype);
+		}
+		// V8: Chrome/Node guard
+		if (typeof Error.captureStackTrace === 'function') {
+			Error.captureStackTrace(this, HttpError);
+		}
+	}
+
+	toJSON() {
+		const { message, stack } = this;
+		return {
+			name: this.name,
+			status: this.status,
+			detail: this.detail,
+			meta: this.meta,
+			message,
+			stack
+		};
+	}
+}
