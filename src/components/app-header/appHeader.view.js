5fd1f5725de2746ab82ac5ad433f212f70c2be7b
yenmangu <robert.shelford@googlemail.com>
2025-11-14 02:32:40 +0000

    chore: fix styles

--- src/components/app-header/appHeader.view.js
+++ src/components/app-header/appHeader.view.js
@@ -9,7 +9,7 @@ export function renderAppHeader(homeLink) {
   <div class="app-header__left">
     <a href="${homeLink}" class="app-header__home-link">
       <img src="./src/../assets/images/logo/cayenne-logo.png" alt="Cayenne Logo" class="app-header__logo me-2" height="28" />
-      <span class="app-header__title fw-bold">Cayenne</span>
+      <span class="app-header__title">Cayenne</span>
     </a>
   </div>
   <div class="app-header__nav-wrapper">
